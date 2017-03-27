1ba42259576d7a4d7e6ac5ce9c47a85abc868dc1
Patti Scalfano <pjs@foundationsoft.local>
2017-03-27 06:35:28 -0400

    After MVC module

---
+++ TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailServices _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailServices mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            // Find a view, render it and return it
+            return View();
+        }
+        public IActionResult About()
+        {
+              return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:To"], model.Email, "From TheWorld", model.Message);
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+
+            }
+            return View();
+        }
+    }
+}
